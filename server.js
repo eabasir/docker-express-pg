3b5c59a84fb1f826c6d9453d1e13a21f92da2205
Ubuntu <ubuntu@ip-172-31-8-250.eu-west-2.compute.internal>
2018-01-20 08:42:38 +0000

    fix server node

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 const PORT = 3000;
 const pg = require('pg');
-pg.connect('postgres://postgres:eabasir@localhost:5432/express_pg_db');
+var pool = new pg.Pool()
+pool.connect('postgres://postgres:eabasir@localhost:5432/express_pg_db');
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 server.get('/', (req, res) => res.status(200).send('hello'));
